e0118847a5c3a2bf068e890eb6250de0392e9480
Shwetay <shwetayeoleravi@gmail.com>
2019-11-02 05:25:48 -0700

    new line added

--- HelloWorldProject/src/com/lti/hello/world/HelloWorld.java
+++ HelloWorldProject/src/com/lti/hello/world/HelloWorld.java
@@ -3,7 +3,8 @@ package com.lti.hello.world;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Git ***********");
+		System.out.println("**********Welcome to Git ***********");
+		System.out.println("*****Added another line*****");
 
 	}
 
